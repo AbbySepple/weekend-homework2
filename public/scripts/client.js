ec0b62ffdea5f96742cbef1125a6dbf37f723011
Abby Sepple <abby.sepple@gmail.com>
2017-04-24 10:05:06 -0500

    update

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,6 +2,41 @@ $(document).ready(function(){
   console.log("client.js is ready.");
 
 
+$('#clear').on('click', clearCalculations);
 
-  
+$('#plus').on('click', function() {
+    console.log("clicked plus");
+    setCalculations ('plus'); //make setCalculations function !!!!!
+  });
+
+$('#minus').on('click', function() {
+    console.log("clicked minus");
+    setCalculations ('minus');
+  });
+
+$('#multiply').on('click', function() {
+    console.log("clicked multiply");
+    setCalculations ('multiply');
+  });
+
+$('#divide').on('click', function() {
+    console.log("clicked divide");
+    setCalculations ('divide');
+  });
+
+
+// FUNCTIONS
+
+function clearCalculations() {
+  $('#containerOne').empty();
+}
+
+function setCalculations() {
+  console.log("setting calculations");
+}
+
+
+
+
+
 });//end document ready
