0405958f839dd5253a2eae858a570d78d701cd89
Abby Sepple <abby.sepple@gmail.com>
2017-04-24 08:16:54 -0500

    basic framework

---
+++ server.js
@@ -0,0 +1,20 @@
+
+
+var result = [];
+
+switch(calculations(x, y)) {
+    case plus:
+        code
+        break;
+    case minus:
+        code
+        break;
+    case multiply:
+        code
+      break;
+    case divide:
+      code
+      break;
+    default:
+        code block
+}
