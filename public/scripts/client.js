47e39319473c3763e1f1311e8d29053b5db296a7
Abby Sepple <abby.sepple@gmail.com>
2017-04-26 18:39:01 -0500

    half way point

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,23 +1,53 @@
-$(document).ready(function(){
-  console.log("client.js is ready.");
-
-
-$('#clear').on('click', clearCalculations);
+$(document).ready(function() {
+    console.log("client.js is ready.");
 
 
 
-// FUNCTIONS
 
-function clearCalculations() {
-  $('#containerOne').empty();
+
+
+    // FUNCTIONS
+    //added class to the divs
+    function clearCalculations() {
+        $('.field').val('');
+    }
+
+    function doThatThing() {
+        console.log("do that thing you do");
+        var valx = $('#xguess').val();
+        var valy = $('#yguess').val();
+        var mFunctions = $('#mathFunctions').val();
+
+        if(isNaN(valx || isNaN(valy))){
+          alert("Stop! I only understand numbers.");
+        }
+
+        var theseNumbers = {
+          x: valx,
+          y: valy,
+          operator: mFunctions
+        }
+
+
+        $.ajax({
+            method: POST,
+            url: '/numbers',
+            data: theseNumbers,
+            success: function(res) {
+                console.log("setting calculations");
+
+            }
+        }); //end ajax
+
+        $('#clear').on('click', clearCalculations);
+
+    } //end doThatThing function
+
+}); //end document ready
+
+
+
+function response(resNum){
+  console.log("in ");
+$('#theResults').text(resNum.answer);
 }
-
-function setCalculations() {
-  console.log("setting calculations");
-}
-
-
-
-
-
-});//end document ready
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var app = express();
 
 
+app.use(express.static('public'));
+
 app.listen(3007, function() {
 console.log("in the year 3007");
 });
